d271a25b9df1e3bde314dae340f5e19c18c6007c
Serhii Taran <serhii.taran@planetvpn.dev>
2025-09-10 22:49:26 +0300

    fix: variable area to circleArea

--- jsBasics/task4.js
+++ jsBasics/task4.js
@@ -1,9 +1,9 @@
 // task 4.1
 let radius = 5;
-const area = Math.PI * Math.pow(radius, 2);
+const circleArea = Math.PI * Math.pow(radius, 2);
 
 console.log("Radius:", radius);
-console.log("Area:", area.toFixed(2));
+console.log("Area:", circleArea.toFixed(2));
 
 // task 4.2
 const length = 4;
