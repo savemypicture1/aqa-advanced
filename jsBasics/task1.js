3bb553e59253c4c62d33164cc81d9e5a309bf118
Serhii Taran <serhii.taran@planetvpn.dev>
2025-09-10 22:12:21 +0300

    feat: add hw task1

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+const message = "Hello, world!";
+const age = 25;
+const isActive = false;
+const emptyValue = null;
+const notAssigned = undefined;
+
+console.log(message);
+console.log(age);
+console.log(isActive);
+console.log(emptyValue);
+console.log(notAssigned);
