ce3a7e819b2751f728f00068bda694745c41fd6d
Serhii Taran <serhii.taran@planetvpn.dev>
2025-09-10 22:13:05 +0300

    feat: add hw task4

---
+++ jsBasics/task4.js
@@ -0,0 +1,24 @@
+// task 4.1
+let radius = 5;
+const area = Math.PI * Math.pow(radius, 2);
+
+console.log("Radius:", radius);
+console.log("Area:", area.toFixed(2));
+
+// task 4.2
+const length = 4;
+const width = 10;
+const rectangleArea = length * width;
+
+console.log("Length:", length);
+console.log("Width:", width);
+console.log("Rectangle Area:", rectangleArea.toFixed(2));
+
+// task 4.3
+radius = 4;
+const height = 7;
+const cylinderVolume = Math.PI * Math.pow(radius, 2) * height;
+
+console.log("Radius:", radius);
+console.log("Height:", height);
+console.log("Cylinder Volume:", cylinderVolume.toFixed(2));
